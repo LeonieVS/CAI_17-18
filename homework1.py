559da3ed43b77aee8759bb4c1a594d5ee4873845
LeonieVS <s0140973@ad.ua.ac.be>
2018-02-14 10:26:39 +0100

    cleaned up

--- homework1.py
+++ homework1.py
@@ -1,13 +1,10 @@
 def fibonacci():
-    fibo = [1, 1]
     prevnum = 1
     num = 1
-    while num < 4000000:
+    addition = 0
+    while num <= 4000000:
         prevnum, num = num, num + prevnum
-        fibo.append(num)
-    fourmil = 0
-    for i in fibo:
-        if i % 2 == 0:
-            fourmil =+ i
-    return fourmil
+        if num % 2 == 0:
+            addition += num
+    return addition
 print(fibonacci())
